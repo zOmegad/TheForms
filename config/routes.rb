95c3ca6742c6c67d5494c33877aa3fc35bfe9f77
zOmegad <simonrichard1998@gmail.com>
2018-10-30 17:12:10 +0100

    app done, yeah

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  resources :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :users 
   get "/index", to: "users#index"
   get "/new", to: "users#new"
   get "/show", to: "users#show"
+  get "/show/:id", to: "users#show"
+  get "/", to: "users#index"
 end
